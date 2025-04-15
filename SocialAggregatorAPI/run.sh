f826f57b3f467eb7e14764031fa62c706553129a
tsts83 <tsts83@gmail.com>
2025-04-15 23:17:46 +0200

    docker fxed

--- SocialAggregatorAPI/run.sh
+++ SocialAggregatorAPI/run.sh
@@ -5,7 +5,11 @@ set -a
 source .env
 set +a
 
-docker run -it \
+# Pull the latest image from DockerHub
+docker pull tsts83/socialaggregatorapi:latest
+
+# Run the container from DockerHub with necessary environment variables
+docker run -it --rm \
   -p 8080:8080 \
   -e DEFAULT_CONNECTION="$DEFAULT_CONNECTION" \
   -e JWT_KEY="$JWT_KEY" \
@@ -14,5 +18,4 @@ docker run -it \
   -e ADMIN_API_KEY="$ADMIN_API_KEY" \
   -e MINIAPP_USER_PASSWORD="$MINIAPP_USER_PASSWORD" \
   tsts83/socialaggregatorapi:latest
-
-    
+    sh -c "echo 'Starting application...'; dotnet SocialAggregatorAPI.dll"

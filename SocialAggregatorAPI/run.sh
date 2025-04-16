ef555baac2c94309bde0a543e9a3c5bab0e8ea57
tsts83 <tsts83@gmail.com>
2025-04-16 12:55:01 +0200

    docker fixes

--- SocialAggregatorAPI/run.sh
+++ SocialAggregatorAPI/run.sh
@@ -9,13 +9,6 @@ set +a
 docker pull tsts83/socialaggregatorapi:latest
 
 # Run the container from DockerHub with necessary environment variables
-docker run -it --rm \
-  -p 8080:8080 \
-  -e DEFAULT_CONNECTION="$DEFAULT_CONNECTION" \
-  -e JWT_KEY="$JWT_KEY" \
-  -e NEWSDATA_API_KEY="$NEWSDATA_API_KEY" \
-  -e HUGGINGFACE_API_KEY="$HUGGINGFACE_API_KEY" \
-  -e ADMIN_API_KEY="$ADMIN_API_KEY" \
-  -e MINIAPP_USER_PASSWORD="$MINIAPP_USER_PASSWORD" \
-  tsts83/socialaggregatorapi:latest
-    sh -c "echo 'Starting application...'; dotnet SocialAggregatorAPI.dll"
+docker run --env-file .env -it --rm -p 8080:8080 -v ${PWD}:/app tsts83/socialaggregatorapi:latest
+
+    # sh -c "echo 'Starting application...'; dotnet SocialAggregatorAPI.dll"
